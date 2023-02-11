053d0ad6863e5ac25c0af539effa9a389ed551be
praksahb <39479839+praksahb@users.noreply.github.com>
2023-02-12 02:24:23 +0530

    not working- created class for board and struct for cell

--- displayBoard.h
+++ displayBoard.h
@@ -16,7 +16,6 @@ class DisplayBoard
 {
     const int maxXrows, maxYcols;
     std::vector<std::vector<char>> board;
-    vector<vector<bool>> visited;
     GameBoard *gboard;
     int winCondition;
     int visitedNodes;
@@ -28,8 +27,7 @@ class DisplayBoard
 public:
     DisplayBoard(int x, int y, int mines) : maxXrows(x),
                                             maxYcols(y),
-                                            board(maxXrows, vector<char>(maxYcols, ' ')),
-                                            visited(maxXrows, vector<bool>(maxYcols, false))
+                                            board(maxXrows, vector<char>(maxYcols, ' '))
     {
         winCondition = maxXrows * maxYcols - mines;
         gboard = new GameBoard(x, y, mines);
@@ -48,8 +46,7 @@ public:
 
     void initializeGameBoard(int x, int y)
     {
-        gboard->fillMines({x, y});
-        gboard->incrementAdjacents();
+        gboard->fillMines();
         gameState = on;
         checkCellDFS(x, y);
     }
@@ -69,123 +66,56 @@ public:
             // check if valid location
             if (x >= 0 && x < maxXrows && y >= 0 && y < maxYcols)
             {
-                // check if location not yet checked
-                if (visited[x][y] == false)
-                {
-                    // check value from gboard
-                    int val = gboard->getCellValue(x, y);
+                // check value from gboard
+                CellNode val = gboard->getCellValue(x, y);
 
-                    // Case 1 - blank cell
-                    if (val == mempty)
-                    {
-                        board[x][y] = '0';
-                        visited[x][y] = true;
-                        if (++visitedNodes == winCondition)
-                        {
-                            updateAllMines();
-                            gameState = gameWon;
-                        }
-                        // add all 8 adjacent x,y values to stack
-                        for (int i = 0; i < 8; i++)
-                        {
-                            int adjx = x + directionX[i];
-                            int adjy = y + directionY[i];
-                            stk.push({adjx, adjy});
-                        }
-                    }
+                // check if location not yet checked
+                if (val.visited == false)
+                {
+                    val.visited = true;
+                    visitedNodes++;
+
                     // Case 2 - bomb in cell
-                    else if (val == mine)
+                    if (val.isBomb == true)
                     {
-                        updateAllMines();
-                        gameState = off;
+                        // updateAllMines();
+                        gameState = gameLost;
                         return;
                     }
-                    // Case 3 - Number in cell
+                    // Case 1 - blank cell
                     else
                     {
-                        // value is no. of bombs around it (maxVal == 8)
-                        board[x][y] = static_cast<char>(val + '0');
-                        visited[x][y] = true;
-                        if (++visitedNodes == winCondition)
+
+                        if (val.adjacentBombs == 0)
                         {
-                            updateAllMines();
-                            gameState = gameWon;
+                            board[x][y] = '0';
+                            if (visitedNodes == winCondition)
+                            {
+                                // updateAllMines();
+                                gameState = gameWon;
+                            }
+                            // add all 8 adjacent x,y values to stack
+                            for (int i = 0; i < 8; i++)
+                            {
+                                int adjx = x + directionX[i];
+                                int adjy = y + directionY[i];
+                                stk.push({adjx, adjy});
+                            }
+                        }
+                        // Case 3 - Number in cell
+                        else
+                        {
+                            // value is no. of bombs around it (maxVal == 8)
+                            board[x][y] = static_cast<char>(val.adjacentBombs + '0');
+                            if (visitedNodes == winCondition)
+                            {
+                                // updateAllMines();
+                                gameState = gameWon;
+                            }
                         }
                     }
                 }
             }
-        }
-    }
-
-    void updateAllMines()
-    {
-        // use stack of mines to display all the mines.
-        // try without using a copy of stack
-        stack<pair<int, int>> temp = gboard->getMines();
-        while (!temp.empty())
-        {
-            pair<int, int> xy = temp.top();
-            board[xy.first][xy.second] = 'X';
-            temp.pop();
-        }
-    }
-
-    // recursive function - not working
-    void checkCell(int x, int y)
-    {
-        // check if x and y are legal
-        if (x < 0 || y < 0 || x >= maxXrows || y >= maxYcols)
-        {
-            return;
-        }
-
-        if (visited[x][y] == true)
-        {
-            return;
-        }
-
-        // get value of board[x][y]
-        int val = gboard->getCellValue(x, y);
-
-        // not needed
-        if (val == -2)
-        {
-            // invalid cell location
-            return;
-        }
-
-        // val == bomb
-        if (val == -1)
-        {
-            // bomb opened
-            gameState = off;
-            board[x][y] = 'X';
-            visited[x][y] = true;
-            visitedNodes++;
-            return;
-        }
-
-        if (val == 0)
-        {
-            // empty cell opened
-            board[x][y] = '0';
-            visited[x][y] = true;
-            visitedNodes++;
-
-            // open adjacent cells
-            for (int i = 0; i < 8; i++)
-            {
-                int adjx = directionX[i];
-                int adjy = directionY[i];
-                checkCell(adjx, adjy);
-            }
-        }
-        else
-        {
-            // val = no. of mines in adjacency
-            board[x][y] = static_cast<char>(val + '0');
-            visited[x][y] = true;
-            visitedNodes++;
         }
     }
 
